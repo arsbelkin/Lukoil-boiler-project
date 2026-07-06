502414874b32f2a77fe384df73b4a45fbd567861
Egor Kozlov <egork0929@gmail.com>
2026-07-07 01:21:13 +0300

    OPC UA сервер - заготовка и теги

---
+++ src/opc_server.py
@@ -0,0 +1,17 @@
+from asyncua.sync import Server
+from opc_tags import TAGS
+
+
+#class OPCBoilerServer:
+#    def __init__(self, endpoint="opc.tcp://0.0.0.0:4840/freeopcua/server/"): #работаем по протоколу TCP из документации дальше как подключаемся к серваку 
+#
+ #   def _setup_nodes(self):
+       
+  #  def start(self):
+        
+   # def stop(self):
+        
+    #def get_value(self, name):
+        
+    #def set_value(self, name, value): 
+        
