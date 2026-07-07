bbea75248248a42a55a0b3cf46c67710ae9648de
Egor Kozlov <egork0929@gmail.com>
2026-07-07 10:36:11 +0300

    Написал файлы, взял реализацию у Егора

--- src/opc_server.py
+++ src/opc_server.py
@@ -1,17 +1,33 @@
+
 from asyncua.sync import Server
 from opc_tags import TAGS
 
 
-#class OPCBoilerServer:
-#    def __init__(self, endpoint="opc.tcp://0.0.0.0:4840/freeopcua/server/"): #работаем по протоколу TCP из документации дальше как подключаемся к серваку 
-#
- #   def _setup_nodes(self):
-       
-  #  def start(self):
-        
-   # def stop(self):
-        
-    #def get_value(self, name):
-        
-    #def set_value(self, name, value): 
-        
+class OPCBoilerServer:
+    def __init__(self, endpoint="opc.tcp://0.0.0.0:4840/freeopcua/server/"):
+        self.server = Server()                          
+        self.server.set_endpoint(endpoint)              
+        uri = "http://example.org/boiler/"
+        self.idx = self.server.register_namespace(uri)  
+        self.nodes = {}
+        self._setup_nodes()
+
+    def _setup_nodes(self):
+        boiler = self.server.nodes.objects.add_object(self.idx, "Boiler")
+        for name, props in TAGS.items():
+            node = boiler.add_variable(self.idx, name, props["initial"])
+            if props.get("writable", False):
+                node.set_writable()
+            self.nodes[name] = node
+
+    def start(self):
+        self.server.start()                             
+
+    def stop(self):
+        self.server.stop()
+
+    def get_value(self, name):
+        return self.nodes[name].read_value()
+
+    def set_value(self, name, value):
+        self.nodes[name].write_value(value)
