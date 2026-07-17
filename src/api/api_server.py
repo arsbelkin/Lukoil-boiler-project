fc996762549cee869284f3d7fa997e53073c730a
arsbelkin <arseniibelkin@gmail.com>
2026-07-17 15:53:49 +0300

    test-api graph

--- src/api/api_server.py
+++ src/api/api_server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi import status
@@ -7,6 +7,10 @@ from opc import OPCBoilerClient
 from .api_model import SetValueDTO
 
 from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import io
 
 app = FastAPI()
 
@@ -33,3 +37,29 @@ def get_data():
 def set_value(dto: SetValueDTO):
     client.set_value(dto.get_name(), dto.value)
     return status.HTTP_200_OK
+
+
+@app.get("/api/v1/graph")
+def get_graph():
+    g = create_graph()
+
+    return Response(content=g.getvalue(), media_type="image/png")
+
+
+
+def create_graph():
+    X = np.linspace(-7, 17, 10000)
+    Y1 = X ** 2
+    Y2 = X ** 3
+
+    fig, ax = plt.subplots(nrows=2, ncols=1)
+    ax[0].plot(X, Y1)
+    ax[1].plot(X, Y2)
+
+    buf = io.BytesIO()
+    fig.savefig(buf, format="png", bbox_inches='tight')
+
+    plt.close(fig)
+
+    buf.seek(0)
+    return buf
