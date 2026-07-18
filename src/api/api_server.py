bb36104e4a259a008aa1eee33764ae3f0b1b5b00
arsbelkin <arseniibelkin@gmail.com>
2026-07-18 18:38:37 +0300

    api controllers file

--- src/api/api_server.py
+++ src/api/api_server.py
@@ -1,68 +1,29 @@
-from fastapi import FastAPI, Response
-from fastapi.responses import FileResponse
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi import status
+
+from contextlib import asynccontextmanager
 
 from opc import OPCBoilerClient
-from .api_model import SetValueDTO
 
 from pathlib import Path
 
-import matplotlib.pyplot as plt
-import numpy as np
-import io
-
-app = FastAPI()
 
 client = OPCBoilerClient()
-client.connect()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    client.connect()
+    yield
+    client.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.mount("/ui", StaticFiles(directory=BASE_DIR / "ui"), name="ui")
 
 
-@app.get("/", response_class=FileResponse)
-def index():
-    html_path = BASE_DIR / "ui" / "index.html"
-    return FileResponse(html_path, media_type="text/html")
+from .controllers import router
 
-
-@app.get("/api/v1/data")
-def get_data():
-    return client.get_data()
-
-
-@app.post("/api/v1/data")
-def set_value(dto: SetValueDTO):
-    try:
-        client.set_value(dto.name, dto.value)
-        return status.HTTP_200_OK
-    except Exception as e:
-        print(e)
-        return status.HTTP_400_BAD_REQUEST
-
-
-@app.get("/api/v1/graph")
-def get_graph():
-    g = create_graph()
-
-    return Response(content=g.getvalue(), media_type="image/png")
-
-
-def create_graph():
-    X = np.linspace(-7, 17, 10000)
-    Y1 = X**2
-    Y2 = X**3
-
-    fig, ax = plt.subplots(nrows=2, ncols=1)
-    ax[0].plot(X, Y1)
-    ax[1].plot(X, Y2)
-
-    buf = io.BytesIO()
-    fig.savefig(buf, format="png", bbox_inches="tight")
-
-    plt.close(fig)
-
-    buf.seek(0)
-    return buf
+app.include_router(router)
---
+++ src/api/controllers.py
@@ -0,0 +1,59 @@
+from .api_server import BASE_DIR, client
+from .api_model import SetValueDTO
+
+from fastapi import APIRouter, Response
+from fastapi.responses import FileResponse
+from fastapi import status
+
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+
+
+router = APIRouter()
+
+
+@router.get("/", response_class=FileResponse)
+def index():
+    html_path = BASE_DIR / "ui" / "index.html"
+    return FileResponse(html_path, media_type="text/html")
+
+
+@router.get("/api/v1/data")
+def get_data():
+    return client.get_data()
+
+
+@router.post("/api/v1/data")
+def set_value(dto: SetValueDTO):
+    try:
+        client.set_value(dto.name, dto.value)
+        return status.HTTP_200_OK
+    except Exception as e:
+        print(e)
+        return status.HTTP_400_BAD_REQUEST
+
+
+@router.get("/api/v1/graph")
+def get_graph():
+    g = create_graph()
+
+    return Response(content=g.getvalue(), media_type="image/png")
+
+
+def create_graph():
+    X = np.linspace(-7, 17, 10000)
+    Y1 = X**2
+    Y2 = X**3
+
+    fig, ax = plt.subplots(nrows=2, ncols=1)
+    ax[0].plot(X, Y1)
+    ax[1].plot(X, Y2)
+
+    buf = io.BytesIO()
+    fig.savefig(buf, format="png", bbox_inches="tight")
+
+    plt.close(fig)
+
+    buf.seek(0)
+    return buf
