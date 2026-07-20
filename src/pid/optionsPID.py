46f6cb4edbb1344dac1b47ce10f39d2008387acf
arsbelkin <arseniibelkin@gmail.com>
2026-07-20 23:53:16 +0300

    fix: params pid

--- src/pid/optionsPID.py
+++ src/pid/optionsPID.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class PIDOptions:
-    kp: float = 2.0
-    ki: float = 0.5
-    kd: float = 1.0
+    kp: float = 2.1
+    ki: float = 0.7
+    kd: float = 0.6
     output_limits: tuple = (0.0, 1.0)
 
     dt: float = 1.0
